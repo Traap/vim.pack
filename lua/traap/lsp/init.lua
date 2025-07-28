aabca2214358593694505d043d49b1d09e1bd164
Traap <gary.a.howard@mac.com>
2025-07-28 12:16:23 -0700

    Add lsp_servers.lua stub.

--- lua/traap/lsp/init.lua
+++ lua/traap/lsp/init.lua
@@ -1,24 +1,6 @@
 -- Enable LSP
 
-local language_servers = {
-  biome = { name = 'bitbake-language-server', },
-  cpp = { name = 'clangd', },
-  csharp = { name = 'csharp-ls', },
-  css = { name = 'cssls', },
-  emmet = { name = 'emmetls', },
-  python = { name = 'pyright', },
-  go = { name = 'gopls', },
-  json = { name = 'jsonls', },
-  lua = { name = 'lua_ls', },
-  ruby = { name = 'solargraph', },
-  rust = { name = 'rust_analyzer', },
-  sh = { name = 'bashls', },
-  tinymist = { name = 'tinymist', },
-  sql = { name = 'sqlls', },
-  tex = { name = 'texlab', },
-  typescript = { name = 'tsserver', },
-  yaml = { name = 'yamlls', },
-}
+local language_servers = require("traap.config.lsp_servers").language_servers
 
 -- Collect server names
 local enabled_servers = {}
