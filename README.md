161d965bbb277095edfc2e4e728bfa92e9ed0035
Traap <gary.a.howard@mac.com>
2025-07-28 12:08:11 -0700

    Correcgt pyright file name.

--- README.md
+++ README.md
@@ -5,4 +5,15 @@ This is a work-in-progress.
 
 https://gpanders.com/blog/whats-new-in-neovim-0-11/
 
+TODO  I'd like each lsp/server-name.lua file to follow a patter similar to the
+one below.
+```lua
+return {
+  local servers = require("traap.config.lsp_servers")
+  local server = servers.name["yaml"]
+  cmd = server.cmd,
+  root_markers = server.root_markers,
+  filetypes = server.filetypes
+}
+```
 Traap - 2025-07-28
--- lua/traap/lsp/init.lua
+++ lua/traap/lsp/init.lua
@@ -1,88 +1,30 @@
 -- Enable LSP
 
-local filetype_to_server = {
-  biome = {
-    name = 'biome',
-    filetypes = { 'javascript', 'typescript', 'json' },
-  },
-  cpp = {
-    name = 'clangd',
-    filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
-  },
-  csharp = {
-    name = 'omnisharp',
-    filetypes = { 'cs', 'vb' },
-  },
-  css = {
-    name = 'cssls',
-    filetypes = { 'css', 'less', 'scss' },
-  },
-  emmet = {
-    name = 'emmetls',
-    filetypes = {
-      "astro", "css", "eruby", "html", "htmlangular", "htmldjango",
-      "javascriptreact", "less", "pug", "sass", "scss",
-      "svelte", "typescriptreact", "vue",
-    },
-  },
-  python = {
-    name = 'pyright',
-    filetypes = { 'python' },
-  },
-  go = {
-    name = 'gopls',
-    filetypes = { 'go', 'gomod' },
-  },
-  json = {
-    name = 'jsonls',
-    filetypes = { 'json', 'jsonc' },
-  },
-  lua = {
-    name = 'lua_ls',
-    filetypes = { 'lua' },
-  },
-  ruby = {
-    name = 'solargraph',
-    filetypes = { 'ruby' },
-  },
-  rust = {
-    name = 'rust_analyzer',
-    filetypes = { 'rust' },
-  },
-  sh = {
-    name = 'bashls',
-    filetypes = { 'sh', 'bash', 'zsh' },
-  },
-  tinymist = {
-    name = 'tinymist',
-    filetypes = { 'typst' },
-  },
-  sql = {
-    name = 'sqlls',
-    filetypes = { 'sql' },
-  },
-  tex = {
-    name = 'texlab',
-    filetypes = { 'bib', 'latex', 'tex' },
-  },
-  typescript = {
-    name = 'tsserver',
-    filetypes = {
-      'javascript', 'javascriptreact',
-      'typescript', 'typescriptreact',
-    },
-  },
-  yaml = {
-    name = 'yamlls',
-    filetypes = { 'yaml', 'yml' },
-  },
+local language_servers = {
+  biome = { name = 'bitbake-language-server', },
+  cpp = { name = 'clangd', },
+  csharp = { name = 'csharp-ls', },
+  css = { name = 'cssls', },
+  emmet = { name = 'emmetls', },
+  python = { name = 'pyright', },
+  go = { name = 'gopls', },
+  json = { name = 'jsonls', },
+  lua = { name = 'lua_ls', },
+  ruby = { name = 'solargraph', },
+  rust = { name = 'rust_analyzer', },
+  sh = { name = 'bashls', },
+  tinymist = { name = 'tinymist', },
+  sql = { name = 'sqlls', },
+  tex = { name = 'texlab', },
+  typescript = { name = 'tsserver', },
+  yaml = { name = 'yamlls', },
 }
 
 -- Collect server names
 local enabled_servers = {}
 local seen = {}
 
-for _, entry in pairs(filetype_to_server) do
+for _, entry in pairs(language_servers) do
   if entry.name and not seen[entry.name] then
     table.insert(enabled_servers, entry.name)
     seen[entry.name] = true
