3fab04db7b7b67721e26e1a4cd64eabd4f800e48
Traap <gary.a.howard@mac.com>
2026-08-08 12:01:54 -0700

    Add LspInfo to support muscle memory.

--- lua/traap/config/cmd.lua
+++ lua/traap/config/cmd.lua
@@ -5,3 +5,7 @@ vim.cmd("colorscheme tokyonight-night")
 vim.api.nvim_create_user_command("Lazy", function()
   vim.pack.update()
 end, { desc = "Update plugins with vim.pack" })
+
+vim.api.nvim_create_user_command("LspInfo", function()
+  vim.cmd("checkhealth vim.lsp")
+end, { desc = "Show native LSP health information" })
